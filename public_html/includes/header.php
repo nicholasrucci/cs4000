b57cc25c82b19bf2ba13ffb2310874d7fdc10a7a
Nicholas Rucci <nicholasrucci@krypton.local>
2015-09-16 07:53:42 -0600

    Configure nav bar in header.php

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -46,14 +46,14 @@ if (!$conn) {
           </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul class="nav navbar-nav">
-                <li class=""><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
-                <li><a href="multiplication.php">Multiplicaiton</a></li>
-                <li><a href="function.php">Function</a></li>
+                <li class=""><a href="../index.php">Home <span class="sr-only">(current)</span></a></li>
+                <li><a href="../game/index.php">Games</a></li>
+                <li><a href="../game/new.php">Add a Game</a></li>
                 <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Playground Projects<span class="caret"></span></a>
                   <ul class="dropdown-menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
+                    <li><a href="../multiplication.php">Multiplication</a></li>
+                    <li><a href="../function.php">Function</a></li>
                     <li><a href="#">Something else here</a></li>
                     <li role="separator" class="divider"></li>
                     <li><a href="#">Separated link</a></li>
@@ -61,7 +61,7 @@ if (!$conn) {
                     <li><a href="#">One more separated link</a></li>
                   </ul>
                 </li>
-                <li><a href="contact.php">Contact</a></li>
+                <li><a href="../contact.php">Contact</a></li>
               </ul>
               <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Link</a></li>
