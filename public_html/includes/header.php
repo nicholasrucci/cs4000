b933cded764207218fb0ea947f64aa7d93838cf6
Nicholas Rucci <nicholasrucci@krypton.local>
2015-09-18 00:08:15 -0600

    theme change

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -24,7 +24,7 @@ if (!$conn) {
 <html> 
   <head>
     <base href="https://whispering-lowlands-3713.herokuapp.com/public_html/">
-    <link rel="stylesheet" href="https://bootswatch.com/cyborg/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/paper/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <title>PHP Includes</title> 
