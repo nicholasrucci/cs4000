df7c7a804acb64f8de35f1631cb6548067bd55e7
Nicholas Rucci <nicholasrucci@ip-144-38-189-254.dixie.edu>
2015-10-07 11:37:39 -0600

    Logout

--- public_html/sessions/destroy.php
+++ public_html/sessions/destroy.php
@@ -1,12 +1,18 @@
+<?php require_once("../includes/header.php") ?>
 
+<?php if (isset($_SESSION['user_id'])): ?>
 
-<?php
+    <?php
 
-if (isset($_SESSION['user_id'])) {
     unset($_SESSION['user_id']);
-    header('Location: ./public_html');
-} else {
-    header('Location: ./public_html');
-}
+    header('Location: index.php');
 
-?>
+    ?>
+
+<?php else: ?>
+
+    <?php header('Location: index.php'); ?>
+
+<?php endif ?>
+
+<?php require_once("../includes/footer.php") ?>
