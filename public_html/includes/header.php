78ef51a190950efd77e17d39ce6b3c57bdfe644a
Nicholas Rucci <nicholasrucci@ip-144-38-189-254.dixie.edu>
2015-10-07 11:16:45 -0600

    Filter index

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -71,7 +71,7 @@ function printStr(&$str) {
                 <li><a href="contact.php">Contact</a></li>
               </ul>
               <ul class="nav navbar-nav navbar-right">
-                <?php if (isset($_SESSION['id'])): ?>
+                <?php if (isset($_SESSION['user_id'])): ?>
                   <li><a href="sessions/destroy">Logout</a></li>
                 <?php else: ?>
                   <li><a href="sessions/new.php">Login</a> </li>
