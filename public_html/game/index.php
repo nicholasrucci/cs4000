dcf422adce3845395f4ba6e9b1e1cec43c5c1f11
Nicholas Rucci <nicholasrucci@ip-144-38-189-254.dixie.edu>
2015-10-07 10:25:01 -0600

    Filter index

--- public_html/game/index.php
+++ public_html/game/index.php
@@ -1,8 +1,40 @@
 <?php require_once("../includes/header.php"); ?>
 
+<p>
+    <a href="index.php">All Games</a>
+    <a href="index.php?filter=title">By Title</a>
+    <a href="index.php?filter=genre">By Genre</a>
+</p>
+
+<p>
+<form method="get" action="index.php">
+    Sort by:
+    <input type="checkbox" id="filter_title" name="filter_title" value="title">
+    <label for="sort_age">Age</label>
+    <br>
+    Filter by:
+    <input type="checkbox" id="filter_genre" name="filter_genre" value="genre">
+    <label for="filter_children">Children</label>
+    <br>
+    Search:
+    <input type="text" name="search">
+    <br>
+    <input type="submit" name="submit" value="Go">
+</form>
+</p>
+
 <?php
 
     $sql = "SELECT * FROM games";
+
+    if (isset($_GET['filter']) && $_GET['filter_title'] == 'title') {
+        $sql .= " ORDER BY title";
+    }
+
+    if (isset($_GET['sort']) && $_GET['filter_genre'] == 'genre') {
+        $sql .= " ORDER BY genre";
+    }
+
     $result    = mysqli_query($conn, $sql);
 
 ?>
@@ -12,6 +44,7 @@
     There was a problem: <?= $error ?>
 
 <?php else: ?>
+
 
 <table class='table table-striped table-hover'>
     <tr>
