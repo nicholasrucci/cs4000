556351696188bdc15cf7198d68257d10a4556fbe
Nicholas Rucci <nicholasrucci@ip-144-38-189-254.dixie.edu>
2015-10-09 12:29:28 -0600

    display user email

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -72,7 +72,7 @@ function printStr(&$str) {
               </ul>
               <ul class="nav navbar-nav navbar-right">
                 <?php if (isset($_SESSION['user_id'])): ?>
-                  <li><a href="sessions/destroy.php"><?= $_SESSION['email'] ?></a></li>
+                  <li><?= $_SESSION['email'] ?></li>
                   <li><a href="sessions/destroy.php">Logout</a></li>
                 <?php else: ?>
                   <li><a href="sessions/new.php">Login</a> </li>
