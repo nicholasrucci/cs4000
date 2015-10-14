2a5f22987cb46542d00663f058c0799045478142
Nick <ld.tech@outlook.com>
2015-10-14 12:16:45 -0600

    if value is set for form

--- public_html/game/new.php
+++ public_html/game/new.php
@@ -15,15 +15,15 @@
 <form class="form-horizontal" method="post" action="game/create.php" enctype="multipart/form-data">
     <p>
         <label for="title">Game Title</label><br>
-        <input type="text" id="title" name="title">
+        <input type="text" id="title" name="title" value="<?php echo isset($_POST['title']) ? $_POST['title'] : '' ?>">
     </p>
     <p>
         <label for="genre">Genre</label><br>
-        <input type="text" id="genre" name="genre">
+        <input type="text" id="genre" name="genre" value="<?php echo isset($_POST['genre']) ? $_POST['genre'] : '' ?>>
     </p>
     <p>
         <label for="creator">Creator</label><br>
-        <input type="text" id="creator" name="creator">
+        <input type="text" id="creator" name="creator" value="<?php echo isset($_POST['creator']) ? $_POST['creator'] : '' ?>>
     </p>
     <p>
         <input class="btn btn-primary" type="submit" name="submit" value="Add Game">
